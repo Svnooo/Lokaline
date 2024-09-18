3c45d73346aebe70bd7130370a6a57e2add9319f
SonnyRilman <rilmansonny@gmail.com>
2024-09-18 23:50:07 +0700

    test

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+const Content = () => {
+    return (
+      <div className="mockup-window border-base-500 border">
+  <div className="border-base-300 flex justify-center border-t px-4 py-16">Hello!</div>
+</div>
+    )
+}
+
+export default Content;

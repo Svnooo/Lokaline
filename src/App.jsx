44c0904620979527ded2c3b4a74e6916d37ffbae
Svnooo <arielstevano17@gmail.com>
2024-09-20 00:26:39 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Navbar from './components/Dassboard/Navbar';
 import Content from './components/Dassboard/Content';
 import Footer from './components/Dassboard/Footer';
 import Login from './components/Dassboard/Login';
+import { NavbarWithMegaMenu } from './components/Dassboard/Navbar';
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <>
+      <NavbarWithMegaMenu/>
       <Content />
       <Footer />
       <Login />
-    </div>
+    </>
   );
 }
 

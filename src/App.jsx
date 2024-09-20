067792ca02d98dd43b7912a06ba537a75c527227
SonnyRilman <rilmansonny@gmail.com>
2024-09-20 22:43:56 +0700

    fotterk

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// Ubah impor dari 'react-router-dom'
+import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { NavbarWithMegaMenu } from './components/Navbar';
 import BantuandanDukungan from './pages/BantuandanDukungan/BantuandanDukungan';
@@ -12,24 +12,31 @@ import PelatihandanWebinar from './pages/PelatihandanWebinar/PelatihandanWebinar
 import ProfileUMKM from './pages/ProfileUMKM/ProfileUMKM';
 import Register from './pages/Register/Register';
 import Testimoni from './pages/Testimoni/Testimoni';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <NavbarWithMegaMenu />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/BantuandanDukungan" element={<BantuandanDukungan />} />
-        <Route path="/BeritadanArtikel" element={<BeritadanArtikel />} />
-        <Route path="/Catalog" element={<Catalog />} />
-        <Route path="/ForumDiskusi" element={<ForumDiskusi />} />
-        <Route path="/KontakdanLokasi" element={<KontakdanLokasi />} />
-        <Route path="/PelatihandanWebinar" element={<PelatihandanWebinar />} />
-        <Route path="/ProfileUMKM" element={<ProfileUMKM />} />
-        <Route path="/Testimoni" element={<Testimoni />} />
-      </Routes>
+      <div id="root">
+        <NavbarWithMegaMenu />
+        <main>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/BantuandanDukungan" element={<BantuandanDukungan />} />
+            <Route path="/BeritadanArtikel" element={<BeritadanArtikel />} />
+            <Route path="/Catalog" element={<Catalog />} />
+            <Route path="/ForumDiskusi" element={<ForumDiskusi />} />
+            <Route path="/KontakdanLokasi" element={<KontakdanLokasi />} />
+            <Route path="/PelatihandanWebinar" element={<PelatihandanWebinar />} />
+            <Route path="/ProfileUMKM" element={<ProfileUMKM />} />
+            <Route path="/Testimoni" element={<Testimoni />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,58 @@
 // src/components/Footer.jsx
-import React from 'react';
+"use client";
+import React from "react";
+import { Footer } from "flowbite-react";
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 
-const Footer = () => {
+export default function FooterComponent() {
   return (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto text-center">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
-        <p>
-          Contact us: <a href="mailto:info@yourcompany.com" className="hover:text-gray-400">info@yourcompany.com</a>
-        </p>
+    <Footer container>
+      <div className="w-full">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div>
+            <Footer.Brand
+              href="https://flowbite.com"
+              src="https://flowbite.com/docs/images/logo.svg"
+              alt="Flowbite Logo"
+              name="LOCALINE"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Localine</Footer.Link>
+                <Footer.Link href="#">Tailwind CSS</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Github</Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms & Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="Localine™" year={2024} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
       </div>
-    </footer>
+    </Footer>
   );
-};
-
-export default Footer;
+}
